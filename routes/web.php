646f37e639bbd3bde7a372509fa34db50fe50416
aditya <akstr17coding@gmail.com>
2020-09-01 03:05:09 +0530

    second commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,33 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pizzas', function () {
+    // $pizza = [
+    //     'type'=>'hawaiian', 
+    //     'base' => 'cheesy crust',
+    //     'price' => 10 
+    // ];
+    // return view('pizzas', $pizza);
+    $pizzas = [
+        ['type'=> 'hawaiian',  'base' => 'cheesy crust','price' => 10],
+        ['type'=> 'volcano',  'base' => 'Garlic crust','price' => 20],
+        ['type'=> 'veg Supreme',  'base' => 'Thin & crispy','price' => 30]
+    ];
+
+
+
+        return view('pizzas' , [
+            'pizzas'=> $pizzas,
+            'name' =>  request('name'),
+            'age' =>request('age')
+        ]);
+
+});
+
+Route::get('/pizzas/{id}', function ($id) {
+  
+        return view('details' , ['id' => $id]);
+
+});
+
