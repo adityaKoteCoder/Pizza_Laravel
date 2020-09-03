0f177a71d1a869861bbe07c3bb36c9ecfafdaa66
aditya <akstr17coding@gmail.com>
2020-09-04 03:07:14 +0530

    fifth commit

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -41,10 +41,18 @@ class PizzaController extends Controller
         $pizza->name = request('name');
         $pizza->type = request('type');
         $pizza->base = request('base');
+        $pizza->toppings = request('toppings');
 
         $pizza -> save();
 
         return redirect('/')-> with('mssg', 'ThankFull for your order');
+    }
+
+    public function destroy($id){
+        $pizza = Pizza::findOrFail($id);
+        $pizza->delete();
+        
+        return redirect('/pizzas');
     }
 
 
